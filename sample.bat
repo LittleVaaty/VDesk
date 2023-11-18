48f63abd394031cfd45792bd22f7c20ea8b4b3c0
LittleVaaty <hansen-jonathan@hotmail.com>
2023-11-18 22:10:47 +0100

    Add sample file

---
+++ sample.bat
@@ -0,0 +1,6 @@
+﻿start VDesk.exe create -v 4
+start VDesk.exe set-name -o 2 test-vdesk
+start VDesk.exe run -o 1 --half-split right notepad -a "/A sample.bat"
+start VDesk.exe switch 3
+timeout /t 1
+start VDesk.exe move -o 2 -n notepad
