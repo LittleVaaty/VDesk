6621ac024df6f9a0f5379c2252eb504f9d02ecbc
Jonathan Hansen <hansen-jonathan@hotmail.com>
2024-02-23 09:28:01 +0100

    Remove old project

--- VirtualDesktopCore/VirtualDesktopExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using WindowsDesktop.Interop.Proxy;
-
-namespace WindowsDesktop;
-
-public static class VirtualDesktopExtensions
-{
-    internal static VirtualDesktop ToVirtualDesktop(this IVirtualDesktop desktop)
-        => VirtualDesktop.FromComObject(desktop);
-}
